ae96286ae7e1d7a39cda09073490237fccd13862
Vincenzo De Naro Papa <vincenzo.denaropapa@amazee.io>
2024-06-20 17:22:31 +0200

    Validation

--- various/validation.tf
+++ various/validation.tf
@@ -8,3 +8,23 @@ variable "list_example" {
       error_message = "Too few elements"
     }
 }
+
+variable "test_text" {
+    description = "Example of text content"
+    type = string
+    default = "test-1"
+    validation {
+	condition = substr(var.test_text, 0, 4) == "test"
+	error_message = "Variable must start with test"
+    }
+}
+
+variable "test_text2" {
+    description = "Example of text content"
+    type = string
+    default = "test-1"
+    validation {
+	condition = substr(var.test_text2, 0, 4) == "test" && length(var.test_text2) >= 5
+	error_message = "Variable must start with test and lenght more/equal than 5"
+    }
+}
