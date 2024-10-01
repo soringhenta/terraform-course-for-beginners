7ba27562bfc49a0ea2b494095ce9b9b5d4611040
Vincenzo De Naro Papa <vincenzo.denaropapa@amazee.io>
2024-10-01 23:56:32 +0200

    Fix gcp example

--- helloworld/gcp/main.tf
+++ helloworld/gcp/main.tf
@@ -1,19 +1,19 @@
 provider "google" {
-  project = "corsoterraform"
+  project = "tfundamental"
   region  = "europe-west3"
   zone    = "europe-west3-a"
 }
 
 resource "google_compute_instance" "gcp-instance" {
   name         = "gcp-instance2"
-  machine_type = "e2-standard-2"
+  machine_type = "e2-micro"
   boot_disk {
     initialize_params {
       image = "ubuntu-minimal-2404-noble-amd64-v20240608"
     }
   }
   labels = {
-    name = "HelloWorld"
+    name = "helloworld"
   }
 
   network_interface {
